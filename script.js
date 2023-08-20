6a4bfafdcbefdf9ba928ecad05ed7153af839456
Tejas Fatkar Patil <tejasfatkar2002@gmail.com>
2023-08-20 19:11:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+liObj = document.getElementById("win");
+liObj1 = document.getElementById("win1")
+liObj2 = document.getElementById("win2")
+liObj3 = document.getElementById("win3")
+liObj4 = document.getElementById("win4")
+liObj5 = document.getElementById("win5")
+heightObj = document.getElementById("sixth-sec")
+render();
+function addtext() {
+    liObj.style.display = "block";
+    heightObj.style.height = "fit-content";
+    render();
+}
+function addtext1() {
+    liObj1.style.display = "block";
+    heightObj.style.height = "fit-content";
+    render();
+}
+function addtext2() {
+    liObj2.style.display = "block";
+    heightObj.style.height = "fit-content";
+    render();
+}
+function addtext3() {
+    liObj3.style.display = "block";
+    heightObj.style.height = "fit-content";
+    render();
+}
+function addtext4() {
+    liObj4.style.display = "block";
+    heightObj.style.height = "fit-content";
+    render();
+}
+function addtext5() {
+    liObj5.style.display = "block";
+    heightObj.style.height = "fit-content";
+    render();
+}
